c26c2288cb5df675fabda60c5fe54335233060d7
Catalina Gonzalez <catalinagonzalez1410@gmail.com>
2022-02-21 14:58:33 -0300

    testing with react library

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,30 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders probando react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/probando/i);
-  expect(linkElement).toBeInTheDocument();
+describe('when everything is OK', () => {
+  
+  test('should render the App component without crashing', () => {
+    render(<App />);
+    screen.debug();
+  })
+// Testeamos que exista o no una palabra en el documento con try & catch
+  // test('should select the children that is being passed to the CustomInput component', () => {
+  //   render(<App />);
+  //   screen.getByText('Input:');
+  //   let error;
+  //   try {
+  //     screen.getByText('Input')
+  //   } catch(err) {
+  //     error = err;
+  //   }
+  //   expect(error).toBeDefined();
+  // })
+
+  test('should select the children that is being passed to the CustomInput component', () => {
+    render(<App />);
+    screen.getByText('Input:');
+//  screen.getByText(/Input/);
+//  Podemos poner una regular expression en vez de un string
+  })
+
 });
