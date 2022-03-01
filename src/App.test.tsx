ffdf34c1da0986247a4b453df7d8ca80a4d39c78
CatalinaGonzalez <catalinagonzalez1410@gmail.com>
2022-03-01 13:38:18 -0300

    testing with all

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -31,25 +31,32 @@ describe('when everything is OK', () => {
 
   test('should select the children that is being passed to the CustomInput component', () => {
     // render(<App />); con beforeEach nos ahorramos repetir esta línea
-    screen.getByText('Input:');
+    // screen.getByText('Input:');
+    screen.getAllByText('Input:'); //Debemos usar el All porque duplicamos el input, por ende debe buscar varios elementos, no solo uno.
 //  screen.getByText(/Input/);
 //  Podemos poner una regular expression en vez de un string
   })
 
   test('should select the input element by its role', () => {
     // render(<App />); con beforeEach nos ahorramos repetir esta línea
-    screen.getByRole('textbox'); // textbox equivale al role del input
-    expect(screen.getByRole('textbox')).toBeInTheDocument();
+    // textbox equivale al role del input
+    // screen.getByRole('textbox'); 
+    screen.getAllByRole('textbox'); //También debemos usar All aquí
+    // expect(screen.getByRole('textbox')).toBeInTheDocument();
+    expect(screen.getAllByRole('textbox')[0]).toBeInTheDocument(); //Recibirá un array de inputs, por eso necesitamos especificar una posicion para testear.
+    expect(screen.getAllByRole('textbox')[1]).toBeInTheDocument();
+    expect(screen.getAllByRole('textbox').length).toEqual(2);
   })
 
   test('should select a label element by its text', () => {
     // render(<App />); con beforeEach nos ahorramos repetir esta línea
-    screen.getByLabelText('Input:');
+    screen.getByLabelText('Input:'); //Aquí no es necesario usar All porque react library solo leerá una label
   })
 
   test('should select input element by placeholder test', () => {
     // render(<App />); con beforeEach nos ahorramos repetir esta línea
-    screen.getByPlaceholderText('Example');
+    // screen.getByPlaceholderText('Example');
+    screen.getAllByPlaceholderText('Example'); //Aquí también debemos usar All
   })
 
   test('should select the input element by its role with queryByRole', () => {
